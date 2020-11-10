f4c7f11afce1ebdbfe30a1bc40953183a80ae0dc
unknown <markaustin017@gmail.com>
2020-11-11 02:41:40 +0800

    Responsive Navbar

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,11 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Noto Sans', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 body{
   margin:0%;
--- src/views/Navbar.vue
+++ src/views/Navbar.vue
@@ -1,14 +1,29 @@
 <template>
     <div id = "Body">
         <div id = "navBox">
+            <div id = "title">
+                <h1>ICpEP.SE-Mapua Valorant Act II</h1>
+            </div>
             <div id = "links">
-            <router-link id="link" to="/Contact">Contact</router-link>
-            <router-link id="link" to="/About">About</router-link>
-            <router-link id="link" to="/Standings">Standings</router-link>
-            <router-link id="link" to="/Schedule">Schedule</router-link>
-            <router-link id="link" to="/Home">Home</router-link>
+            <router-link v-if="mobileView >=750" id="link" to="/Home">Home</router-link>
+            <router-link v-if="mobileView >=750" id="link" to="/Schedule">Schedule</router-link>
+            <router-link v-if="mobileView >=750" id="link" to="/Standings">Standings</router-link>
+            <router-link v-if="mobileView >=750" id="link" to="/About">About</router-link>
+            <router-link v-if="mobileView >=750" id="link" to="/Contact">Contact</router-link>
+            </div>
+            <div id = "barBox">
+            <i v-if="mobileView < 750" button @click="sideShow = !sideShow" class="fa fa-bars"></i>
             </div>
         </div>
+            <transition name="slide-fade">
+                <div id = "sideBar" v-if="sideShow">
+                <router-link  id="link" to="/Home">Home</router-link>
+                <router-link  id="link" to="/Schedule">Schedule</router-link>
+                <router-link  id="link" to="/Standings">Standings</router-link>
+                <router-link  id="link" to="/About">About</router-link>
+                <router-link  id="link" to="/Contact">Contact</router-link>
+                </div>
+            </transition>
         <div id = "content">
             <router-view />
         </div>
@@ -18,7 +33,25 @@
 <script>
 export default {
     name:'Navbar',
+    data(){
+        return{
+            mobileView:window.innerWidth,
+            sideShow:false,
+        }
+    },
+    created() {
+        window.addEventListener("resize", this.changeSize);
+    },
+    destroyed() {
+        window.removeEventListener("resize", this.changeSize);
+    },
+    methods:{
+        changeSize(){
+            this.mobileView=window.innerWidth;
+        }
+    }
 }
+
 </script>
 
 <style lang="scss" scoped>
@@ -26,15 +59,50 @@ export default {
     top: 0%;
     overflow:hidden;
     background-color: aqua;
+    display:flex;
+    #title{
+        h1{
+            padding: 0.75% 0 0.5% 1vw;
+            font-size: 20px;
+            background-color: darkgreen;
+        }
+    }
     #links{
-
+        margin-left: auto;
+        background-color: darkgrey;
+        text-align: right;
+        margin-top: 20px;
         #link{
-            float:right;
-            margin-right:0%;
             text-align: center;
-            padding:1.5vw;
+            padding: 50% 2vw 50% 2vw;
             background-color: crimson;
         }
+    }
+    #barBox{
+        i{
+            background-color: darkblue;
+            padding: 20px;
+        }
+    }
+}
+.slide-fade-enter-active {
+transition: all .3s ease;
+}
+.slide-fade-leave-active {
+transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-to
+{
+transform: translateX(10px);
+opacity: 0;
+}
+#sideBar{
+    float:right;
+    display:grid;
+    background-color: darkmagenta;
+    width:100%;
+    #link{
+        padding:3%;
     }
 }
 </style>
