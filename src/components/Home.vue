1e1f8d9718b7b8381b8e87c755682ef4a0488089
unknown <markaustin017@gmail.com>
2020-11-11 14:22:27 +0800

    Added logo and Registration on home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,47 @@
 <template>
-    <div>
-        <p>Home</p>
+    <div id="homeBox">
+        <img alt="Valo logo" src="../assets/logo.png">
+        <div id="dateBox">
+            <h1>{{dateRange}}</h1>
+        </div>
+        <div id="regBox">
+            <a href="https://forms.gle/aQBrTTwFWAogu9SGA">Register</a>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
     name:"Home",
+    date(){
+        return{
+            dateRange:"NOV 23 - NOV 27",
+        }
+    },
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+#homeBox{
+    img{
+        display:block;
+        padding: 3%;
+        margin: auto;
+        width:500px;
+        background-color: brown;
+    }
+    #regBox{
+        background-color: aqua;
+        margin: auto;
+        width:100%;
+        max-width: 300px;
+        padding: 30px 5%;
+        border-radius: 10px;
+        a{
+            margin-left: 40%;
+        }
+    }
+}
 </style>
+
 
