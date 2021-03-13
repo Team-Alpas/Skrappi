125e28b56822ce9c67bc1e82ce344b24d6a6c539
markang812 <markang812@gmail.com>
2021-03-13 21:59:10 +0800

    edited about page and added a font

--- src/App.vue
+++ src/App.vue
@@ -12,8 +12,9 @@ export default {
 
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Kanit:wght@200;300;400;500;600;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap');
 #app {
-  font-family: 'Kanit', Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Kanit', Avenir, Helvetica, Arial, sans-serif,'Montserrat', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
