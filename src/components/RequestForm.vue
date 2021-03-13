9c9e82349b0aa85d73b092c90aedc3d01b105d9b
unknown <markaustin017@gmail.com>
2021-03-14 00:59:37 +0800

    Request Front Done | Maui

--- src/components/RequestForm.vue
+++ src/components/RequestForm.vue
@@ -15,6 +15,7 @@
                 <input type='text' id='inForm' name='itemDesc' placeholder='Item Description'><br>
                 <input type='text' id='inForm' name='Quantity' placeholder='Quantity'><br>
                 <input type='text' id='inForm' name='location' placeholder='Your Location'><br>
+                <h3>Post Request</h3>
             </div>
         </div>
         <img id="wave" alt="wave" src="../assets/wave.png">
@@ -51,25 +52,52 @@ export default {
         height:400px;
         display:flex;
         margin-top:40px;
+
         padding-top:20px;
-        background-color: #a8d44c;
+        background-color: #4dd964;
         #mapBox{
-            margin: auto;
+            margin:auto;
         }
         #formBox{
-            width:25%;
-            height:300px;
-            top:70px;
-            right:20%;
+            margin-right:35%;
             #inForm{
+                width:300%;
+                height:30px;
+                border-radius: 5px;
+                border-color: transparent;
                 padding:5%;
                 min-width: 100px;
                 max-width: 100vw;
+                margin: 5%;
+            }
+            h3{
+                text-align: center;
+                width:100%;
+                height:20px;
+                font-family: Arial;
+                font-style: normal;
+                font-weight: 500;
+                font-size: calc(5px + 1vw);
+                color: white;
+                margin-top: 5px;
+                border-radius: 20px;
+                margin-left:190%;
+                padding:calc(1px + 1vw);
+                border-color:white;
+                border-style: solid;
+                border-width: thin;       
+                &:hover {
+                    transition: 0.3s ease-in-out;
+                    background: white;
+                    //background: #4dd964;
+                    color:#4dd964;
+                }   
             }
         }
     }
     img{
         width:100%;
+        filter: invert(81%) sepia(15%) saturate(1953%) hue-rotate(70deg) brightness(93%) contrast(82%);
     }
 }
 </style>
