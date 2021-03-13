f0d8c400080e8ff685ee4d1861806a7a1e21a94c
markang812 <markang812@gmail.com>
2021-03-13 15:57:22 +0800

    updated login and sign up pages

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,7 +1,19 @@
 <template>
-    <div>
-        <p>Auction</p>
-    </div>    
+    <div id='body'>
+        <div id="login_txt">
+            <p>Log in</p>
+        </div>
+        <div id='form_area'>
+        <form>
+            <input type='text' id='email' name='email' placeholder='Email address'><br>
+            <input type='text' id='password' name='password' placeholder='Password'><br>
+
+        </form>
+        </div>
+        <p> Don't have an account yet? <router-link  id="link" to="/signup">Sign Up</router-link></p>
+        <button>Log In</button>
+        
+    </div>
 </template>
 
 <script>
@@ -10,7 +22,43 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang='scss' scoped>
 
+
+#body{
+    padding-left: 15%;
+    padding-top: 2%;
+    padding-bottom: 15%;
+    min-height: 100%;
+    background-color:#66883F;
+    color: #efe1ba;
+}
+#login_txt{
+    p{
+        font-size: 2.5rem;
+        color: #EFE1BA;
+    }
+}
+
+input{
+        margin-top: 0.75rem;
+        margin-bottom: 0.75rem;
+        width:60%;
+        padding: 1.5%;
+        box-sizing: border-box;
+        font-size: 1.75rem;
+    }
+
+
+button {
+  background-color: #4056A1;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin-bottom: 4%;
+}
 </style>
-
